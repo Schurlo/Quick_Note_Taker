5bf49a064c1901d1c2bc8426baccea94741a3c35
Schurlo <georg.engelsmann@gmail.com>
2024-03-11 12:46:15 +0100

    JSON-Objekt

--- Quick_Note_Taker/Program.cs
+++ Quick_Note_Taker/Program.cs
@@ -4,25 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Quick_Note_Taker
 {
+    class NotizObjekt
+    {
+        public string DatumZeit { get; set; }
+        public string Notiz { get; set; }
+    }
+
     internal class Program
     {
-        static string file = "Notizen.txt"; 
+        static List<string> notizenList = new List<string>();
+        static string file = "Notizen.txt";
 
         static void Main(string[] args)
         {
+            notizenList = File.ReadAllLines(file).ToList();
+
             while (true)
             {
                 string notiz = Console.ReadLine();
 
-                if (!notiz.Contains("/") && !notiz.Contains("|")  && !string.IsNullOrWhiteSpace(notiz))
+                if (IsValid(notiz))
                 {
-                    string dateTime = DateTime.Now.ToString("dd-MM-yy HH:mm");
-                    string format = $"{dateTime} : {notiz} |";
+                    var dateTime = DateTime.Now.ToString("dd-MM-yy HH:mm");
 
-                    File.AppendAllText(file, format);
+                    NotizObjekt obj = new NotizObjekt()
+                    {
+                        DatumZeit = dateTime,
+                        Notiz = notiz
+                    };
+
+                    List<string> list = new List<string>();
+
+                    string jsonString = JsonConvert.SerializeObject(obj);
+                    list.Add(jsonString);
+                    File.AppendAllLines(file, list);
                 }
                 else
                 {
@@ -56,44 +75,55 @@ namespace Quick_Note_Taker
                         case "/clear":
                             Console.Clear();
                             break;
+
+                        case "/close":
+                            Environment.Exit(0);
+                            break;
                     }
                 }
             }
 
         }
 
+        static bool IsValid(string notiz)
+        {
+            return !notiz.StartsWith("/") && !notiz.Contains("|") && !string.IsNullOrWhiteSpace(notiz);
+        }
+
         static void Read()
         {
-            string[] fileSplit = Split();
+            int anzahl = 0;
 
-            foreach(string line in fileSplit)
+            try
             {
-                Console.WriteLine(line);
+                string[] fileSplit = File.ReadAllLines(file);
+
+                for (int i = 0; i < fileSplit.Length; i++)
+                {
+                    NotizObjekt notiz = JsonConvert.DeserializeObject<NotizObjekt>(fileSplit[i]);
+                    Console.WriteLine(notiz.DatumZeit + ": " + notiz.Notiz);
+                    anzahl++;
+                }
+
+                Console.WriteLine($"Sie haben {anzahl} Notizen" + Environment.NewLine);
             }
-            Console.WriteLine($"Sie haben {fileSplit.Length - 1} Notizen" + Environment.NewLine);
+            catch
+            {
+                Console.WriteLine($"Kein Dokument unter {file} gefunden");
+            }           
         }
 
         static void Delete(string StrID)
         {
             int id = Convert.ToInt32(StrID) - 1;
 
-            string[] fileSplit = Split();
+            string[] fileSplit = File.ReadAllLines(file);
             var list = fileSplit.ToList();
             list.Remove(fileSplit[id]);
 
             File.Delete(file);
-            foreach(string s in list)
-            {
-                File.AppendAllText(file, s);
-            }
-        }
-
-        static string[] Split()
-        {
-            string file = File.ReadAllText(Program.file);
-            string[] fileSplit = file.Split('|');
-
-            return fileSplit;
+            File.AppendAllLines(file, list);
+            Console.WriteLine($"Notiz {id} wurde gelöscht");
         }
     }
 }
